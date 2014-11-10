405e423936eec3002c27451423fcc987fac83ddf
sMoKeSz <iuly_zep_pelin@yahoo.com>
2014-11-10 16:26:37 +0200

    add basic auth

--- src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
+++ src/main/java/ro/teamnet/z2h/web/ZeroToHeroServlet.java
@@ -1,11 +1,15 @@
 package ro.teamnet.z2h.web;
 
+
+import org.apache.tomcat.util.buf.Base64;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.io.PrintWriter;
+import java.util.Enumeration;
 
 
 public class ZeroToHeroServlet extends HttpServlet {
@@ -23,8 +27,32 @@ public class ZeroToHeroServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
          resp.setContentType("text/html");
          resp.getWriter().write(handleRequest(req));
+         PrintWriter out = resp.getWriter();
+        Enumeration headerNames = req.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = (String) headerNames.nextElement();
+            out.print("<br/>Header Name: <em>" + headerName);
+            String headerValue = req.getHeader(headerName);
+            out.print("</em>, Header Value: <em>" + headerValue);
+            out.println("</em>");
+        }
 
+
+        out.println("<hr/>");
+        String authHeader = req.getHeader("authorization");
+        String encodedValue = authHeader.split(" ")[1];
+        out.println("Base64-encoded Authorization Value: <em>" + encodedValue);
+
+        String decodedValue = Base64.base64Decode(encodedValue);
+
+        out.println("</em><br/>Base64-decoded Authorization Value: <em>" + decodedValue);
+        out.println("</em>");
 
     }
 
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
 }
